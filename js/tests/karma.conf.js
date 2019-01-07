dddfded0e9c2d57932203470dd6bd4394c79539a
XhmikosR <xhmikosr@gmail.com>
2019-01-07 03:08:39 +0200

    Update karma.conf.js

--- js/tests/karma.conf.js
+++ js/tests/karma.conf.js
@@ -1,15 +1,14 @@
 /* eslint-env node */
 /* eslint no-process-env: 0 */
 
+const path = require('path')
 const ip = require('ip')
-const pkg = require('../../package.json')
 const {
   browsers,
   browsersKeys
 } = require('./browsers')
-const path = require('path')
-const jsCoveragePath = path.resolve(__dirname, '../coverage')
 
+const jsCoveragePath = path.resolve(__dirname, '../coverage')
 const jqueryFile = process.env.USE_OLD_JQUERY ? 'https://code.jquery.com/jquery-1.9.1.min.js' : 'node_modules/jquery/dist/jquery.slim.min.js'
 const bundle = process.env.BUNDLE === 'true'
 const browserStack = process.env.BROWSER === 'true'
